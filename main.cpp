89a06dcad03f77e2ca0fb79d3266a4e071385bea
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2022-09-03 16:25:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,59 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-struct cookie{
-    int t;
-    int* a; int* b; int* n; int* m;
+typedef long long ll;
 
-    cookie(){
-        cin>>t;
-        a=new int[t];
-        b=new int[t];
-        n=new int[t];
-        m=new int[t];
-        for (int i=0; i<t; i++){
-            cin>>a[i]>>b[i]>>n[i]>>m[i];
+struct cookie {
+    int t;
+    ll* a; ll* b; ll* n; ll* m;
+
+    cookie() {
+        cin >> t;
+        a = new ll[t];
+        b = new ll[t];
+        m = new ll[t];
+        n = new ll[t];
+        for (int i = 0; i < t; i++) {
+            cin >> a[i] >> b[i] >> n[i] >> m[i];
         }
+    }
+
+    void ang() {
+        for (int i = 0; i < t; i++) {
+            //viendo si hay suficientes galletas.
+            if (a[i] + b[i] < m[i] + n[i]) {
+                std::cout << "NO"<<std::endl;
+                continue;
+            }
+            //viendo cual es el valor maximo de galletas
+            if (a[i] > b[i]) {
+                //viendo si hay suficientes galletas para todos
+                if (b[i] < m[i]) {
+                    std::cout << "NO" << std::endl;
+                    continue;
+                }
+            }
+            else {
+                if (a[i] < m[i]) {
+                    std::cout << "NO" << std::endl;
+                    continue;
+                }
+            }
+            std::cout << "YES" << std::endl;
+
+        }
+    }
+
+    ~cookie() {
+        delete[] a;
+        delete[] b;
+        delete[] n;
+        delete[] m;
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cookie A;
+    A.ang();
     return 0;
 }
